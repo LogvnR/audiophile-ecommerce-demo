51c1d5805af6073c6c87d420c9e01e258681e4d2
Logan Ricard <logvnjs@gmail.com>
2022-10-28 14:19:56 -0500

    added tests from Navbar components and added Navbar components for mobile layout

--- components/Navbar/Navbar.test.tsx
+++ components/Navbar/Navbar.test.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import Navbar from './Navbar';
 
@@ -7,5 +8,23 @@ describe('Navbar', () => {
     expect(() => {
       render(<Navbar />);
     }).not.toThrow();
+  });
+
+  it('renders the main logo', () => {
+    render(<Navbar />);
+    const logo = screen.getByAltText(/audiophile.com/i);
+    expect(logo).toBeInTheDocument();
+  });
+
+  it('renders the hamburger menu', () => {
+    render(<Navbar />);
+    const menu = screen.getByAltText(/side menu/i);
+    expect(menu).toBeInTheDocument();
+  });
+
+  it('renders the cart icon', () => {
+    render(<Navbar />);
+    const cart = screen.getByAltText(/my cart/i);
+    expect(cart).toBeInTheDocument();
   });
 });
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import Image from 'next/image';
+import logo from '../../assets/shared/desktop/logo.svg';
+import menu from '../../assets/shared/tablet/icon-hamburger.svg';
+import cart from '../../assets/shared/desktop/icon-cart.svg';
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-black h-[90px] flex justify-center">
-      <h1 className="text-white">audiophile</h1>
+    <nav className="w-full bg-black h-[90px] flex justify-center items-center px-6 border-b border-[#979797]">
+      <Image src={menu} alt="Side Menu" width={16} height={15} />
+      <div className="w-full flex justify-center items-center">
+        <Image src={logo} alt="audiophile.com" width={143} height={25} />
+      </div>
+      <Image src={cart} alt="My Cart" width={23} height={20} />
     </nav>
   );
 };
