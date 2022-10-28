200162618deff6286eb36615ba1c851ad614a569
Logan Ricard <logvnjs@gmail.com>
2022-10-28 12:03:04 -0500

    updated tailwind config with vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      Manrope: ['Manrope', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'burnt-orange': '#D87D4A',
+        'off-black': '#101010',
+        'light-grey': '#F1F1F1',
+        'off-white': '#FAFAFA',
+        peach: '#FBAF85',
+        error: '#CD2C2C',
+      },
+    },
   },
   plugins: [],
 };
